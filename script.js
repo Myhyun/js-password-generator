724e4a308fb028c5145d7fe32bf1dafb2abfc5d2
Myhyun <mattyihyun@gmail.com>
2020-06-11 16:17:18 -0700

    added variables/prompts/alerts/arrays

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+var spCharsTrue = confirm 
+var numsTrue = confirm 
+var lowerTrue = confirm 
+var upperTrue = confirm 
+
+prompt("Please input password length request (8-128)");
+alert("Include Numbers?");
+alert("Include Special characters?");
+alert("Include Lowercase letters?");
+alert("Include Uppercase letters?");
+
+
+
+
+if (spCharsTrue || lowerTrue || numsTrue || upperTrue === true) {
+  generatePassword
+}
+
+
+
+    alert("Please input a length between 8-128")
+
+
+var spChars = ["!", "@", "#", "$", "%", "^", "&", "*",]
+var numChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
