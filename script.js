44f4c6865a3833b25377f6bec0a83c2538f0a23b
Myhyun <mattyihyun@gmail.com>
2020-06-11 18:02:12 -0700

    reworked confirms/prompts/formatting

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var spChars = ["!", "@", "#", "$", "%", "^", "&", "*",]
+var numChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Write password to the #password input
 function writePassword() {
@@ -15,33 +19,25 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword(){
-  var userInput = prompt("Please input password length desired (8-128)");
+  var pwLength = prompt("Please input password length desired (8-128)");
 
-  if (userInput < 8 || userInput > 128) {
+  if (pwLength < 8 || pwLength > 128) {
     alert("Please input a number between 8 and 128")
   }
 
   else {
-    confirm("Include Special characters?");
-    confirm("Include Numbers?");
-    confirm("Include Lowercase letters?");
-    confirm("Include Uppercase letters?");
+    var spCharsTrue = confirm("Include Special characters?");
+    var numsTrue = confirm("Include Numbers?");
+    var lowerTrue = confirm("Include Lowercase letters?");
+    var upperTrue = confirm("Include Uppercase letters?");
+
+    if (spCharsTrue !==true && numsTrue !==true && lowerTrue !==true && upperTrue !==true){
+      alert("Please allow at least 1 type of character requirement")
+    }
+  
+
+
   }
-  return "password"
 }
-
-
-
-
-
-
-
-var spCharsTrue = confirm
-var numsTrue = confirm
-var lowerTrue = confirm
-var upperTrue = confirm
-
-var spChars = ["!", "@", "#", "$", "%", "^", "&", "*",]
-var numChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//create a master array with the characters that the
+//4 if statesments 1 for each character criteria
