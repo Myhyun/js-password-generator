268236702ff5564cb09fd47a22b4422a41216455
Myhyun <mattyihyun@gmail.com>
2020-06-11 17:15:38 -0700

    added generate password function, reformatted code

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+function generatePassword(){
+  var userInput = prompt("Please input password length desired (8-128)");
 
-var spCharsTrue = confirm 
-var numsTrue = confirm 
-var lowerTrue = confirm 
-var upperTrue = confirm 
+  if (userInput < 8 || userInput > 128) {
+    alert("Please input a number between 8 and 128")
+  }
 
-prompt("Please input password length request (8-128)");
-alert("Include Numbers?");
-alert("Include Special characters?");
-alert("Include Lowercase letters?");
-alert("Include Uppercase letters?");
-
-
-
-
-if (spCharsTrue || lowerTrue || numsTrue || upperTrue === true) {
-  generatePassword
+  else {
+    confirm("Include Special characters?");
+    confirm("Include Numbers?");
+    confirm("Include Lowercase letters?");
+    confirm("Include Uppercase letters?");
+  }
+  return "password"
 }
 
 
 
-    alert("Please input a length between 8-128")
 
+
+
+
+var spCharsTrue = confirm
+var numsTrue = confirm
+var lowerTrue = confirm
+var upperTrue = confirm
 
 var spChars = ["!", "@", "#", "$", "%", "^", "&", "*",]
 var numChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
